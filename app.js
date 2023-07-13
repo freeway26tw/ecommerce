2f78d459081ba0a15b825429873f72bec0c68242
freeway26tw <freeway26tw@gmail.com>
2023-07-13 23:31:40 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,33 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+const session = require('express-session')
+const cors = require('cors')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+const routes = require('./routes')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'secret',
+    resave: false,
+    saveUninitialized: false,
+  })
+)
+app.use(cors())
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use('/api', routes)
+
+app.listen(port, () =>
+  console.log(`Server is listening on port ${port}!
+Press CTRL + C to stop the process.`)
+)
