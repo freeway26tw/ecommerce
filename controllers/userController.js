99782dd8a88368cd22fd7a456cae68c5621081df
freeway26tw <freeway26tw@gmail.com>
2023-07-15 13:04:22 +0800

    style: signUp controller promise to async-await

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,36 +6,32 @@ const prisma = new PrismaClient()
 
 const userController = {
   signUp: async (req, res, next) => {
-    const { email, password, account, name } = req.body
+    try {
+      const { email, password, account, name } = req.body
+      if (password !== req.body.checkPassword)
+        throw createError(400, 'Passwords do not match!')
 
-    if (password !== req.body.checkPassword)
-      throw createError(400, 'Passwords do not match!')
-
-    return await prisma.User.findUnique({
-      where: { account },
-    })
-      .then((user) => {
-        if (user)
-          throw createError(409, 'This account/email is already registered')
-        return bcrypt.hash(password, 10)
+      const user = await prisma.User.findUnique({
+        where: { account },
       })
-      .then(async (hash) => {
-        return await prisma.User.create({
-          data: {
-            email,
-            account,
-            password: hash,
-            name,
-            type: 'User',
-          },
-        })
+      if (user) {
+        throw createError(409, 'This account/email is already registered')
+      }
+      const bcryptPassword = await bcrypt.hash(password, 10)
+      const newUser = await prisma.User.create({
+        data: {
+          email,
+          account,
+          password: bcryptPassword,
+          name,
+          type: 'User',
+        },
       })
-      .then((newUser) => {
-        delete newUser.password
-        console.log(newUser)
-        return res.json(newUser)
-      })
-      .catch((error) => next(error))
+      delete newUser.password
+      return res.json(newUser)
+    } catch (err) {
+      next(err)
+    }
   },
   signIn: (req, res, next) => {
     try {
