e58467bd929c5f9c573c013271b04d2b8ae48132
freeway26tw <freeway26tw@gmail.com>
2023-07-25 00:47:00 +0800

    feat: change user route to login

--- src/config/passport.js
+++ src/config/passport.js
@@ -7,7 +7,7 @@ const JWTStrategy = passportJWT.Strategy
 const ExtractJWT = passportJWT.ExtractJwt
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
-const { checkUserExist } = require('../domains/users/controller.js')
+const { checkUserExist } = require('../domains/login/controller.js')
 
 passport.use(
   new LocalStrategy(
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const userRoute = require('../domains/users')
+const loginRoute = require('../domains/login')
 const productRoute = require('../domains/products')
 const categoryRoute = require('../domains/categories')
 const { apiErrorHandler } = require('../middleware/error-handler')
 
 router.use('/category', categoryRoute)
-router.use('/users', userRoute)
+router.use('/login', loginRoute)
 router.use('/products', productRoute)
 router.use('/', apiErrorHandler)
 
