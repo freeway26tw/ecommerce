b3428ce787aadab2541a19262a31a3cc1d1e251d
freeway26tw <freeway26tw@gmail.com>
2023-07-25 11:39:00 +0800

    feat: home new route

--- src/domains/home/routes.js
+++ src/domains/home/routes.js
@@ -4,14 +4,24 @@ const createError = require('http-errors')
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
-router.get(
-  '/new',
-  async (req, res, next) => {
-    try {
-    } catch (error) {
-      next(error)
-    }
+router.get('/new', async (req, res, next) => {
+  try {
+    const newProduct = await prisma.productVariant.findMany({
+      take: 4,
+      orderBy: [
+        {
+          createdAt: 'desc',
+        },
+      ],
+    })
+    res.json({
+      code: 1,
+      msg: 'Success',
+      result: newProduct
+    })
+  } catch (error) {
+    next(error)
   }
-)
+})
 
 module.exports = router
