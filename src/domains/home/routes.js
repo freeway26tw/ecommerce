53b1fa4e62b751eb3a83ce2ceca45889fd0d7440
freeway26tw <freeway26tw@gmail.com>
2023-07-25 17:02:50 +0800

    feat: home hot route

--- src/domains/home/routes.js
+++ src/domains/home/routes.js
@@ -17,7 +17,27 @@ router.get('/new', async (req, res, next) => {
     res.json({
       code: 1,
       msg: 'Success',
-      result: newProduct
+      result: newProduct,
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/hot', async (req, res, next) => {
+  try {
+    const hotProduct = await prisma.productVariant.findMany({
+      take: 10,
+      orderBy: {
+        OrderDetail: {
+          _count: 'desc'
+        }
+      },
+    })
+    res.json({
+      code: 1,
+      msg: 'Success',
+      result: hotProduct,
     })
   } catch (error) {
     next(error)
