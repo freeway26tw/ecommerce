b27e6fa4bccc73da42052f84fe109fd464aa1cb7
freeway26tw <freeway26tw@gmail.com>
2023-07-26 23:36:49 +0800

    feat: add authenticate

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const createError = require('http-errors')
 
 const loginRoute = require('../domains/login')
 const productRoute = require('../domains/products')
@@ -8,9 +9,20 @@ const goodsRoute = require('../domains/goods')
 const memberRoute = require('../domains/member')
 const homeRoute = require('../domains/home')
 const { apiErrorHandler } = require('../middleware/error-handler')
+const passport = require('../config/passport')
+
+const authenticate = (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err) next(err)
+    if (!user) throw createError(401, 'Your token is invalid.')
+    req.user = user
+    return next()
+  })(req, res, next)
+}
 
 router.use('/login', loginRoute)
 
+// router.use(authenticate)
 router.use('/goods', goodsRoute)
 router.use('/member', memberRoute)
 router.use('/category', categoryRoute)
